445715509294f47596d60cb1c09141b7dbf657fa
kiara-v <michelle_vong@brown.edu>
2024-03-15 00:39:09 -0400

    done lol

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import BakeryItem from "./components/BakeryItem";
 import bakeryData from "./assets/bakery-data.json";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
@@ -11,18 +12,55 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState({});
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+      <div className="content">
+        <main>
+          <h1 className="header">Kiara's Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+          <div className="items">
+            {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+              <BakeryItem // replace with BakeryItem component
+                key={index}
+                item={item}
+                addToCart={() => {
+                  setCart((prevCart) => {
+                    const newCart = { ...prevCart };
+                    newCart[index] = (newCart[index] || 0) + 1;
+                    return newCart;
+                  });
+                }}
+              />
+            ))}
+          </div>
+        </main>
+        <aside>
+          <h2 className="cart-title header">My Cart</h2>
+          {Object.entries(cart).length === 0 ? (
+            <p className="nothing">Nothing here just yet!</p>
+          ) : (
+            <>
+              <ul className="cart-items">
+                {Object.entries(cart).map(([index, quantity]) => {
+                  const item = bakeryData[Number(index)];
+                  return (
+                    <li key={index}>
+                      {quantity}x {item.name}
+                    </li>
+                  );
+                })}
+              </ul>
+              <p className="total header">
+                Total: $
+                {Object.entries(cart).reduce((total, [index, quantity]) => {
+                  const item = bakeryData[Number(index)];
+                  return Number((total + item.price * quantity).toFixed(2));
+                }, 0)}
+              </p>
+            </>
+          )}
+        </aside>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,25 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem({ item, addToCart }) {
+    return (
+          <div className="item">
+            <div className="image">
+              <img
+                src={item.image}
+                alt={item.name}
+              />
+            </div>
+            <div className="desc">
+              <div>
+                <h2 className="item-name">{item.name}</h2>
+                <p>{item.description}</p>
+              </div>
+              <div className="item-cart">
+                <p>${item.price}</p>
+                <button className="add" onClick={addToCart}>
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+    )
+}
